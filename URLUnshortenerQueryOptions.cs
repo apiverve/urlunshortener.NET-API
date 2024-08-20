b6091e678d30f777936ea5ef13268cef10d4da2f
APIVerve Team <hello@apiverve.com>
2024-08-20 11:13:16 -0500

    Bug Fixes

---
+++ URLUnshortenerQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class URLUnshortenerQueryOptions {
+public string url { get; set; }
+}
+
+}
